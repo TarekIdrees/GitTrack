f0f39d07cc75db92d6477af84f8edc4456b8f0d2
TarekIdrees <tarekidrees5@gmail.com>
2024-02-12 21:30:02 +0100

    add needed ui components

---
+++ app/src/main/java/com/tareq/gittrack/ui/common_composables/ContentVisibilty.kt
@@ -0,0 +1,23 @@
+package com.tareq.gittrack.ui.common_composables
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ContentVisibility(
+    state: Boolean,
+    content: @Composable() AnimatedVisibilityScope.() -> Unit
+) {
+    AnimatedVisibility(
+        visible = state,
+        enter = fadeIn(animationSpec = tween(durationMillis = 500)) + scaleIn(),
+        exit = fadeOut(animationSpec = tween(durationMillis = 500))
+    ) {
+        content()
+    }
+}
