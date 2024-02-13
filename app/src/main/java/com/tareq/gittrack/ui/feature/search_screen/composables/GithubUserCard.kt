0b7a16b7f188ab10b7d2c59718bb922f0ae5b8c8
TarekIdrees <tarekidrees5@gmail.com>
2024-02-13 13:53:51 +0100

    redesign github user card

--- app/src/main/java/com/tareq/gittrack/ui/feature/search_screen/composables/GithubUserCard.kt
+++ app/src/main/java/com/tareq/gittrack/ui/feature/search_screen/composables/GithubUserCard.kt
@@ -1,89 +1,173 @@
 package com.tareq.gittrack.ui.feature.search_screen.composables
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.tareq.gittrack.ui.feature.search_screen.GithubUserUi
+import com.tareq.gittrack.ui.theme.GitTrackTheme
 
 @Composable
 fun GithubUserCard(
     modifier: Modifier = Modifier,
     githubUser: GithubUserUi
 ) {
+    var showFullCard by remember {
+        mutableStateOf(false)
+    }
     Card(
-        modifier = modifier,
+        modifier = modifier.animateContentSize(),
         onClick = { /*TODO*/ }
     ) {
-        Row(
-            modifier = Modifier.fillMaxSize(),
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
+        Column (
+            modifier = Modifier.fillMaxWidth()
+        ){
             Image(
                 modifier = Modifier
-                    .size(150.dp)
-                    .clip(RoundedCornerShape(16.dp)),
-                painter = rememberAsyncImagePainter(model = "https://media.istockphoto.com/id/1527223664/nl/foto/the-island-of-kalsoy-hiking-to-kallur-lighthouse-faroe-islands.jpg?s=612x612&w=0&k=20&c=TjL56NeFbEEP0UZHd80jkBJdW9nlpkyRRjuni5MMMIs="),
-                contentDescription = "github user photo",
+                    .fillMaxWidth()
+                    .height(200.dp),
+                painter = rememberAsyncImagePainter(model = githubUser.avatarUrl),
+                contentDescription = "github user image",
                 contentScale = ContentScale.Crop
             )
-            Column {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween
+            Column(
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 20.dp)
+            ) {
+                Text(
+                    text = githubUser.name, maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    style = MaterialTheme.typography.labelLarge
+                )
+                Spacer(modifier = modifier.height(8.dp))
+                Text(
+                    modifier = Modifier.clickable { showFullCard = !showFullCard },
+                    text = githubUser.bio,
+                    maxLines = if (showFullCard) 5 else 2,
+                    overflow = TextOverflow.Ellipsis,
+                    style = MaterialTheme.typography.labelMedium
+                )
+            }
+            Spacer(modifier = modifier.height(30.dp))
+            Row(
+                modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp, bottom = 16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column(
+                    modifier = Modifier.weight(0.8f),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
                 ) {
                     Column {
-                        Text("name")
-                        Text(githubUser.name)
+                        Text("company", style = MaterialTheme.typography.labelSmall)
+                        Spacer(modifier = modifier.height(2.dp))
+                        Text(
+                            githubUser.company,
+                            style = MaterialTheme.typography.labelMedium,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
                     }
                     Column {
-                        Text("bio")
-                        Text(githubUser.bio)
+                        Text("email", style = MaterialTheme.typography.labelSmall)
+                        Spacer(modifier = modifier.height(2.dp))
+                        Text(
+                            githubUser.email,
+                            style = MaterialTheme.typography.labelMedium,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    }
+                    Column {
+                        Text("createdAt", style = MaterialTheme.typography.labelSmall)
+                        Spacer(modifier = modifier.height(2.dp))
+                        Text(
+                            githubUser.createdAt,
+                            style = MaterialTheme.typography.labelMedium,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
                     }
                 }
-                Row(horizontalArrangement = Arrangement.SpaceBetween) {
+                Column(
+                    modifier = Modifier.weight(1.2f),
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                    horizontalAlignment = Alignment.End
+                ) {
                     Column {
-                        Text("company")
-                        Text(githubUser.company)
+                        Text("location", style = MaterialTheme.typography.labelSmall)
+                        Spacer(modifier = modifier.height(2.dp))
+                        Text(
+                            githubUser.location,
+                            style = MaterialTheme.typography.labelMedium,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
                     }
                     Column {
-                        Text("createdAt")
-                        Text(githubUser.createdAt)
-                    }
-                }
-                Row(horizontalArrangement = Arrangement.SpaceBetween) {
-                    Column {
-                        Text("email")
-                        Text(githubUser.email)
-                    }
-                    Column {
-                        Text("location")
-                        Text(githubUser.location)
-                    }
-                }
-                Row(horizontalArrangement = Arrangement.SpaceBetween) {
-                    Column {
-                        Text("following")
-                        Text(githubUser.following)
+                        Text("following", style = MaterialTheme.typography.labelSmall)
+                        Spacer(modifier = modifier.height(2.dp))
+                        Text(
+                            githubUser.following,
+                            style = MaterialTheme.typography.labelMedium,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
                     }
                     Column {
-                        Text("followers")
-                        Text(githubUser.followers)
+                        Text("followers", style = MaterialTheme.typography.labelSmall)
+                        Spacer(modifier = modifier.height(2.dp))
+                        Text(
+                            githubUser.followers,
+                            style = MaterialTheme.typography.labelMedium,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
                     }
                 }
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun GithubUserCardPreview() {
+    GitTrackTheme {
+        GithubUserCard(
+            githubUser = GithubUserUi(
+                name = "TarekIdrees",
+                avatarUrl = "https://avatars.githubusercontent.com/u/58395863?v=4",
+                bio = "Software Engineer worked at company size 10000000",
+                company = "Tempo",
+                email = "tarek.idrees.ad@gmail.com",
+                createdAt = "2019-5-29",
+                location = "Nederland",
+                following = "100",
+                followers = "100"
+            )
+        )
+    }
+
 }
--- app/src/main/java/com/tareq/gittrack/ui/theme/Type.kt
+++ app/src/main/java/com/tareq/gittrack/ui/theme/Type.kt
@@ -1,6 +1,7 @@
 package com.tareq.gittrack.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -14,21 +15,27 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp,
+        color = Color.Gray
+    ),
+    labelMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
+        fontSize = 28.sp,
+        lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
