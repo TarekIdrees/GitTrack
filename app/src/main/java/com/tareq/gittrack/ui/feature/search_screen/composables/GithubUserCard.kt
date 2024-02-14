92e22336db22c2660034271b5695a9a46501e904
TarekIdrees <tarekidrees5@gmail.com>
2024-02-14 23:10:48 +0100

    change card color

--- app/src/main/java/com/tareq/gittrack/ui/feature/search_screen/composables/GithubUserCard.kt
+++ app/src/main/java/com/tareq/gittrack/ui/feature/search_screen/composables/GithubUserCard.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -24,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,160 +44,167 @@ fun GithubUserCard(
     }
     Card(
         modifier = modifier
+            .clip(RoundedCornerShape(16.dp))
             .background(MaterialTheme.colorScheme.surface)
             .animateContentSize(),
         onClick = { /*TODO*/ }
     ) {
-        Column(
-            modifier = Modifier.fillMaxWidth()
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.surface)
         ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(1.5f)
-            ) {
-                Image(
-                    modifier = Modifier.fillMaxSize(),
-                    painter = rememberAsyncImagePainter(model = githubUser.avatarUrl),
-                    contentDescription = "github user image",
-                    contentScale = ContentScale.Crop,
-                    alignment = Alignment.TopStart,
-                )
-            }
             Column(
-                modifier = Modifier.padding(horizontal = 16.dp, vertical = 20.dp)
+                modifier = Modifier.fillMaxWidth()
             ) {
-                Text(
-                    text = githubUser.name, maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                    style = MaterialTheme.typography.labelLarge,
-                    color = MaterialTheme.colorScheme.onSurface
-                )
-                Spacer(modifier = modifier.height(8.dp))
-                Text(
-                    modifier = Modifier.clickable { showFullCard = !showFullCard },
-                    text = githubUser.bio,
-                    maxLines = if (showFullCard) 5 else 2,
-                    overflow = TextOverflow.Ellipsis,
-                    style = MaterialTheme.typography.labelMedium,
-                    color = MaterialTheme.colorScheme.onSurface.copy(0.7f)
-                )
-            }
-            Spacer(modifier = modifier.height(30.dp))
-            Row(
-                modifier
-                    .fillMaxWidth()
-                    .padding(start = 16.dp, end = 16.dp, bottom = 16.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Column(
-                    modifier = Modifier.weight(0.8f),
-                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(1.5f)
                 ) {
-                    Column {
-                        Text(
-                            "company",
-                            style = MaterialTheme.typography.labelSmall,
-                            color = MaterialTheme.colorScheme.onTertiary
-                        )
-                        Spacer(modifier = modifier.height(2.dp))
-                        Text(
-                            githubUser.company,
-                            style = MaterialTheme.typography.labelMedium,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                            color = MaterialTheme.colorScheme.onSurface
-                        )
-                    }
-                    Column {
-                        Text(
-                            "email",
-                            style = MaterialTheme.typography.labelSmall,
-                            color = MaterialTheme.colorScheme.onTertiary
-                        )
-                        Spacer(modifier = modifier.height(2.dp))
-                        Text(
-                            githubUser.email,
-                            style = MaterialTheme.typography.labelMedium,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                            color = MaterialTheme.colorScheme.onSurface
-                        )
-                    }
-                    Column {
-                        Text(
-                            "createdAt",
-                            style = MaterialTheme.typography.labelSmall,
-                            color = MaterialTheme.colorScheme.onTertiary
-                        )
-                        Spacer(modifier = modifier.height(2.dp))
-                        Text(
-                            githubUser.createdAt,
-                            style = MaterialTheme.typography.labelMedium,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                            color = MaterialTheme.colorScheme.onSurface
-                        )
-                    }
+                    Image(
+                        modifier = Modifier.fillMaxSize(),
+                        painter = rememberAsyncImagePainter(model = githubUser.avatarUrl),
+                        contentDescription = "github user image",
+                        contentScale = ContentScale.Crop,
+                        alignment = Alignment.TopStart,
+                    )
                 }
                 Column(
-                    modifier = Modifier.weight(1.2f),
-                    verticalArrangement = Arrangement.spacedBy(8.dp),
-                    horizontalAlignment = Alignment.End,
+                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 20.dp)
+                ) {
+                    Text(
+                        text = githubUser.name, maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        style = MaterialTheme.typography.labelLarge,
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+                    Spacer(modifier = modifier.height(8.dp))
+                    Text(
+                        modifier = Modifier.clickable { showFullCard = !showFullCard },
+                        text = githubUser.bio,
+                        maxLines = if (showFullCard) 5 else 2,
+                        overflow = TextOverflow.Ellipsis,
+                        style = MaterialTheme.typography.labelMedium,
+                        color = MaterialTheme.colorScheme.onSurface.copy(0.7f)
+                    )
+                }
+                Spacer(modifier = modifier.height(30.dp))
+                Row(
+                    modifier
+                        .fillMaxWidth()
+                        .padding(start = 16.dp, end = 16.dp, bottom = 16.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     Column(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalAlignment = Alignment.End
+                        modifier = Modifier.weight(0.8f),
+                        verticalArrangement = Arrangement.spacedBy(8.dp)
                     ) {
-                        Text(
-                            "location",
-                            style = MaterialTheme.typography.labelSmall,
-                            color = MaterialTheme.colorScheme.onTertiary
-                        )
-                        Spacer(modifier = modifier.height(2.dp))
-                        Text(
-                            githubUser.location,
-                            style = MaterialTheme.typography.labelMedium,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                            color = MaterialTheme.colorScheme.onSurface
-                        )
+                        Column {
+                            Text(
+                                "company",
+                                style = MaterialTheme.typography.labelSmall,
+                                color = MaterialTheme.colorScheme.onTertiary
+                            )
+                            Spacer(modifier = modifier.height(2.dp))
+                            Text(
+                                githubUser.company,
+                                style = MaterialTheme.typography.labelMedium,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                                color = MaterialTheme.colorScheme.onSurface
+                            )
+                        }
+                        Column {
+                            Text(
+                                "email",
+                                style = MaterialTheme.typography.labelSmall,
+                                color = MaterialTheme.colorScheme.onTertiary
+                            )
+                            Spacer(modifier = modifier.height(2.dp))
+                            Text(
+                                githubUser.email,
+                                style = MaterialTheme.typography.labelMedium,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                                color = MaterialTheme.colorScheme.onSurface
+                            )
+                        }
+                        Column {
+                            Text(
+                                "createdAt",
+                                style = MaterialTheme.typography.labelSmall,
+                                color = MaterialTheme.colorScheme.onTertiary
+                            )
+                            Spacer(modifier = modifier.height(2.dp))
+                            Text(
+                                githubUser.createdAt,
+                                style = MaterialTheme.typography.labelMedium,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                                color = MaterialTheme.colorScheme.onSurface
+                            )
+                        }
                     }
                     Column(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalAlignment = Alignment.End
+                        modifier = Modifier.weight(1.2f),
+                        verticalArrangement = Arrangement.spacedBy(8.dp),
+                        horizontalAlignment = Alignment.End,
                     ) {
-                        Text(
-                            "following",
-                            style = MaterialTheme.typography.labelSmall,
-                            color = MaterialTheme.colorScheme.onTertiary
-                        )
-                        Spacer(modifier = modifier.height(2.dp))
-                        Text(
-                            githubUser.following,
-                            style = MaterialTheme.typography.labelMedium,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                            color = MaterialTheme.colorScheme.onSurface
-                        )
-                    }
-                    Column(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalAlignment = Alignment.End
-                    ) {
-                        Text(
-                            "followers",
-                            style = MaterialTheme.typography.labelSmall,
-                            color = MaterialTheme.colorScheme.onTertiary
-                        )
-                        Spacer(modifier = modifier.height(2.dp))
-                        Text(
-                            githubUser.followers,
-                            style = MaterialTheme.typography.labelMedium,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                            color = MaterialTheme.colorScheme.onSurface
-                        )
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.End
+                        ) {
+                            Text(
+                                "location",
+                                style = MaterialTheme.typography.labelSmall,
+                                color = MaterialTheme.colorScheme.onTertiary
+                            )
+                            Spacer(modifier = modifier.height(2.dp))
+                            Text(
+                                githubUser.location,
+                                style = MaterialTheme.typography.labelMedium,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                                color = MaterialTheme.colorScheme.onSurface
+                            )
+                        }
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.End
+                        ) {
+                            Text(
+                                "following",
+                                style = MaterialTheme.typography.labelSmall,
+                                color = MaterialTheme.colorScheme.onTertiary
+                            )
+                            Spacer(modifier = modifier.height(2.dp))
+                            Text(
+                                githubUser.following,
+                                style = MaterialTheme.typography.labelMedium,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                                color = MaterialTheme.colorScheme.onSurface
+                            )
+                        }
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.End
+                        ) {
+                            Text(
+                                "followers",
+                                style = MaterialTheme.typography.labelSmall,
+                                color = MaterialTheme.colorScheme.onTertiary
+                            )
+                            Spacer(modifier = modifier.height(2.dp))
+                            Text(
+                                githubUser.followers,
+                                style = MaterialTheme.typography.labelMedium,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                                color = MaterialTheme.colorScheme.onSurface
+                            )
+                        }
                     }
                 }
             }
--- app/src/main/java/com/tareq/gittrack/ui/theme/Color.kt
+++ app/src/main/java/com/tareq/gittrack/ui/theme/Color.kt
@@ -3,7 +3,7 @@ package com.tareq.gittrack.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val background = Color(0xFF484848)
-val card = Color(0xFF121212)
+val card = Color(0xFF252525)
 val Brand = Color(0xFFFEBC33)
 val white30 = Color(0x5EFFFFFF)
 val white87 = Color(0xDEE2E2E2)
