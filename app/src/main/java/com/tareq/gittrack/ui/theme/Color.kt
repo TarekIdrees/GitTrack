376c0a4730340f94244ba08110dd9be3a3dc8c01
TarekIdrees <tarekidrees5@gmail.com>
2024-02-14 20:10:26 +0100

    change system design configurations

--- app/src/main/java/com/tareq/gittrack/ui/theme/Color.kt
+++ app/src/main/java/com/tareq/gittrack/ui/theme/Color.kt
@@ -5,5 +5,5 @@ import androidx.compose.ui.graphics.Color
 val background = Color(0xFF484848)
 val card = Color(0xFF121212)
 val Brand = Color(0xFFFEBC33)
-val white30 = Color(0xCCFFFFFF)
-val white87 = Color(0xFFE2E2E2)
+val white30 = Color(0x5EFFFFFF)
+val white87 = Color(0xDEE2E2E2)
--- app/src/main/java/com/tareq/gittrack/ui/theme/Type.kt
+++ app/src/main/java/com/tareq/gittrack/ui/theme/Type.kt
@@ -1,41 +1,47 @@
 package com.tareq.gittrack.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontFamily
+import com.tareq.gittrack.R
+
+val poppins = FontFamily(
+    listOf(
+        Font(R.font.poppins_regular, FontWeight.Normal),
+        Font(R.font.poppins_bold, FontWeight.Bold),
+        Font(R.font.poppins_medium, FontWeight.Medium),
+        Font(R.font.poppins_semi_bold, FontWeight.SemiBold)
+    )
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = poppins,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = poppins,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp,
-        color = Color.Gray
     ),
     labelMedium = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = poppins,
         fontWeight = FontWeight.SemiBold,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
     ),
     labelLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = poppins,
         fontWeight = FontWeight.Medium,
-        fontSize = 28.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 18.sp,
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = poppins,
+        fontWeight = FontWeight.Medium,
+        fontSize = 22.sp,
     )
 )
