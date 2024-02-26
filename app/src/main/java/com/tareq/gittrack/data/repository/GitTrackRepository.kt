a690745986f33c24a7d653a306041172aa74500b
TarekIdrees <tarekidrees5@gmail.com>
2024-02-26 17:08:51 +0100

    refactor data layer

--- app/src/main/java/com/tareq/gittrack/data/repository/GitTrackRepository.kt
+++ app/src/main/java/com/tareq/gittrack/data/repository/GitTrackRepository.kt
@@ -8,17 +8,13 @@ import com.tareq.gittrack.data.local.toGithubUser
 import com.tareq.gittrack.data.local.toGithubUserEntity
 import com.tareq.gittrack.domain.model.GithubUser
 import com.tareq.gittrack.domain.repository.GitTrackRepository
-import com.tareq.gittrack.domain.util.ForbiddenException
-import com.tareq.gittrack.domain.util.InternalServerException
-import com.tareq.gittrack.domain.util.InvalidDataException
-import com.tareq.gittrack.domain.util.NoConnectionException
-import com.tareq.gittrack.domain.util.NotFoundException
-import com.tareq.gittrack.domain.util.NotFoundOfflineException
+import com.tareq.gittrack.domain.util.ErrorHandler
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEmpty
 import retrofit2.Response
 import java.net.UnknownHostException
 import javax.inject.Inject
@@ -33,25 +29,51 @@ class GitTrackRepositoryImpl @Inject constructor(
         user: String
     ): Flow<GithubUser> {
         val githubUser = wrap(gitTrackApiService.searchGithubUser(user)).toGithubUser()
+        // Caching the data into the database
+        insertGithubUserInDatabase(githubUser)
         return flow { emit(githubUser) }
     }
 
 
     override suspend fun searchGithubUsers(searchTerm: String): Flow<List<GithubUser>> {
         val users = mutableListOf<GithubUser>()
-        wrap(gitTrackApiService.searchGithubUsers(searchTerm)).githubUsers
-            ?.apply {
-                if (this.isEmpty()) throw NotFoundException()
-            }!!.forEach {
-                it.login.takeUnless { login ->
-                    login.isNullOrBlank()
-                }.also { login ->
-                    searchGithubUser(login!!).first { githubUser ->
-                        users.add(githubUser)
+        return flow {
+            try {
+                wrap(gitTrackApiService.searchGithubUsers(searchTerm)).githubUsers
+                    ?.apply {
+                        if (this.isEmpty()) emptyFlow<List<GithubUser>>()
+                    }!!.forEach {
+                        it.login.takeUnless { login ->
+                            login.isNullOrBlank()
+                        }.also { login ->
+                            searchGithubUser(login!!).first { githubUser ->
+                                users.add(githubUser)
+                            }
+                        }
                     }
+                emit(users)
+            } catch (e: Exception) {
+                if (
+                    e is ErrorHandler.Forbidden ||
+                    e is ErrorHandler.NoConnection ||
+                    e is ErrorHandler.UnKnownError ||
+                    e is UnknownHostException
+                ) {
+                    // Get cached data
+                    try {
+                        val githubUsers = getMatchedGithubUsersFromDatabase(searchTerm)
+                        emitAll(githubUsers)
+                    } catch (databaseError: Exception) {
+                        if (e is ErrorHandler.Forbidden)
+                            throw ErrorHandler.ForbiddenAndNotFoundOffline
+                        else
+                            throw databaseError
+                    }
+                } else {
+                    throw e
                 }
             }
-        return flow { emit(users) }
+        }
     }
 
 
@@ -61,7 +83,6 @@ class GitTrackRepositoryImpl @Inject constructor(
 
     override suspend fun getMatchedGithubUsersFromDatabase(userName: String): Flow<List<GithubUser>> {
         return githubUserSourceImpl.getMatchedUsersByName(userName)
-            .onEmpty { throw NotFoundOfflineException() }
             .map {
                 it.map { githubUserEntity ->
                     githubUserEntity.toGithubUser()
@@ -73,30 +94,19 @@ class GitTrackRepositoryImpl @Inject constructor(
     private fun <T> wrap(function: Response<T>): T {
         return try {
             if (function.isSuccessful) {
-                function.body() ?: throw NotFoundException()
+                function.body() ?: throw ErrorHandler.NotFound
             } else {
                 when (function.code()) {
-                    502 -> throw NoConnectionException()
-                    400 -> throw InvalidDataException()
-                    403 -> throw ForbiddenException()
-                    404 -> throw NotFoundException()
-                    500 -> throw InternalServerException()
-                    else -> throw UnknownHostException()
+                    502 -> throw ErrorHandler.NoConnection
+                    400 -> throw ErrorHandler.InvalidData
+                    403 -> throw ErrorHandler.Forbidden
+                    404 -> throw ErrorHandler.NotFound
+                    500 -> throw ErrorHandler.InternalServer
+                    else -> throw ErrorHandler.UnKnownError
                 }
             }
         } catch (error: Exception) {
-            when (function.code()) {
-                502 -> throw NoConnectionException()
-                400 -> throw InvalidDataException()
-                403 -> throw ForbiddenException()
-                404 -> throw NotFoundException()
-                500 -> throw InternalServerException()
-                else -> if (error is UnknownHostException) {
-                    throw NotFoundException()
-                } else {
-                    throw error
-                }
-            }
+            throw ErrorHandler.UnKnownError
         }
     }
 }
