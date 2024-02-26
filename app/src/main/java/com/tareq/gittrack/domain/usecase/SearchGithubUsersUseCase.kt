da2af50c010c99ce71b409ce80e50da7493ca375
TarekIdrees <tarekidrees5@gmail.com>
2024-02-26 17:09:36 +0100

    refactor domain layer

--- app/src/main/java/com/tareq/gittrack/domain/usecase/SearchGithubUsersUseCase.kt
+++ app/src/main/java/com/tareq/gittrack/domain/usecase/SearchGithubUsersUseCase.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
-class SearchGithubUsersOnlineUseCase @Inject constructor(
+class SearchGithubUsersUseCase @Inject constructor(
     private val gitTrackRepository: GitTrackRepository
 ) {
     suspend operator fun invoke(searchTerm: String): Flow<List<GithubUser>> {
